2410f9a9378f4dc53effdb48d5842dd9e7fe4ead
Daniel Kaschel <dipique@gmail.com>
2022-06-21 10:16:47 -0500

    Control access to pages based on whether user is logged in

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -8,6 +8,7 @@ import { AppNavbar } from './AppNavbar'
 import { PersonForm } from './forms/PersonForm'
 import { People } from './pages/People'
 import { Places } from './pages/Places'
+import { LoggedOut } from './pages/LoggedOut'
 
 export const App: FC<{ apiUri: string }> = ({ apiUri }) => {
     const { user, isAuthenticated, isLoading: authLoading, getAccessTokenSilently } = useAuth0()
@@ -16,6 +17,8 @@ export const App: FC<{ apiUri: string }> = ({ apiUri }) => {
     const [ activePage, setActivePage ] = useState('people')
 
     const ActivePage = () => {
+        if (!isAuthenticated)
+            return <LoggedOut />
         switch(activePage)
         {
             case 'people': return <People />
@@ -40,7 +43,7 @@ export const App: FC<{ apiUri: string }> = ({ apiUri }) => {
             })}
         >
             <ActivePage />
-            <button
+            {/* <button
                 disabled={authLoading || isLoading}
                 onClick={async () => {
                     setLoading(true)
@@ -62,7 +65,7 @@ export const App: FC<{ apiUri: string }> = ({ apiUri }) => {
                 }}
             >
                 Call API
-            </button>
+            </button> */}
         </AppShell>
     </MantineProvider>
 }
