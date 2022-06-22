c3cc7f015a616589df3a09375b9b2ced414209cd
Daniel Kaschel <dipique@gmail.com>
2022-06-21 20:55:22 -0500

    Re-enable api button for testing

--- client/src/pages/Places.tsx
+++ client/src/pages/Places.tsx
@@ -1,10 +1,16 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { Title } from '@mantine/core'
+import { useContext, useState } from 'react'
+import { AppContext } from '../app'
 
 export const Places = () => {
+    const { getAccessTokenSilently } = useAuth0()
+    const [ loading, setLoading ] = useState(false)
+    const ctx = useContext(AppContext)
     return <>
         <Title>Places</Title>
-        {/* <button
-                disabled={authLoading || isLoading}
+        <button
+                disabled={!ctx.isAuthenticated || loading}
                 onClick={async () => {
                     setLoading(true)
 
@@ -13,7 +19,7 @@ export const Places = () => {
                             audience: 'dnd-api',
                             scope: 'do:all'
                         })
-                        const response = await fetch(`${apiUri}/download`, {
+                        const response = await fetch(`${ctx.apiUri}/download`, {
                             headers: {
                                 Authorization: `Bearer ${accessToken}`
                             }
@@ -25,6 +31,6 @@ export const Places = () => {
                 }}
             >
                 Call API
-            </button> */}
+            </button>
     </>
 }
