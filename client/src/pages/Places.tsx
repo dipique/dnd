24e8d99b5652e88f713df28678e60c7169310f5a
Daniel Kaschel <dipique@gmail.com>
2022-06-21 20:55:22 -0500

    Re-enable api button for testing

--- client/src/pages/Places.tsx
+++ client/src/pages/Places.tsx
@@ -1,30 +1,36 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { Title } from '@mantine/core'
+import { useContext, useState } from 'react'
+import { AppContext } from '../app'
 
 export const Places = () => {
+    const { getAccessTokenSilently } = useAuth0()
+    const [ loading, setLoading ] = useState(false)
+    const ctx = useContext(AppContext)
     return <>
         <Title>Places</Title>
-        {/* <button
-                disabled={authLoading || isLoading}
-                onClick={async () => {
-                    setLoading(true)
+        <button
+            disabled={!ctx.isAuthenticated || loading}
+            onClick={async () => {
+                setLoading(true)
 
-                    try {
-                        const accessToken = await getAccessTokenSilently({
-                            audience: 'dnd-api',
-                            scope: 'do:all'
-                        })
-                        const response = await fetch(`${apiUri}/download`, {
-                            headers: {
-                                Authorization: `Bearer ${accessToken}`
-                            }
-                        })
-                        console.log('success')
-                    } finally {
-                        setLoading(false)
-                    }
-                }}
-            >
-                Call API
-            </button> */}
+                try {
+                    const accessToken = await getAccessTokenSilently({
+                        audience: 'dnd-api',
+                        scope: 'do:all'
+                    })
+                    const response = await fetch(`${ctx.apiUri}/download`, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`
+                        }
+                    })
+                    console.log('success')
+                } finally {
+                    setLoading(false)
+                }
+            }}
+        >
+            Call API
+        </button>
     </>
 }
