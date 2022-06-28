1892fc4c33b44aa6ff80b45f7bbb089f45b803f1
Daniel Kaschel <dipique@gmail.com>
2022-06-27 20:55:49 -0500

    Create navbar functionality

--- client/src/AppNavbar.tsx
+++ client/src/AppNavbar.tsx
@@ -1,5 +1,26 @@
-import { Navbar } from '@mantine/core'
+import { ActionIcon, Center, createStyles, Navbar, Title } from '@mantine/core'
+import { FC, useContext, useState } from 'react'
+import { Location, MoodBoy } from 'tabler-icons-react'
+import { AppContext } from './app'
 
-export const AppNavbar = () => <Navbar width={{ base: 100 }} p="xs">
-    text
-</Navbar>
+const hoverHighlight = 'rgba(147, 147, 147, 0.1)'
+export const AppNavbar = () => {
+    const { activePage, setActivePage } = useContext(AppContext)
+    const [ hoverPage, setHoverPage ] = useState('')
+
+    const NavbarOption: FC<{ icon: JSX.Element, label: string }> = ({ icon, label }) =>
+        <Center
+            style={(hoverPage === label || activePage === label) ? { background: hoverHighlight } : undefined}
+            onMouseOver={() => setHoverPage(label)}
+            onMouseOut={() => setHoverPage('')}
+            onClick={() => setActivePage(label)}
+        >
+            <ActionIcon size='xl'>{icon}</ActionIcon>
+            <Title style={{ marginLeft: '6px'}} order={6}>{label}</Title>
+        </Center>
+
+    return <Navbar width={{ base: 150 }} p='md'>
+        <NavbarOption icon={<MoodBoy size={50} />} label='people' />
+        <NavbarOption icon={<Location size={50} />} label='places' />
+    </Navbar>
+}
