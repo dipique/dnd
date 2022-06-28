b36555cb06ea96b8c0cb001e154d8468cc46e9d9
Daniel Kaschel <dipique@gmail.com>
2022-06-27 23:56:25 -0500

    tweak navbar styling

--- client/src/AppNavbar.tsx
+++ client/src/AppNavbar.tsx
@@ -4,23 +4,24 @@ import { Location, MoodBoy } from 'tabler-icons-react'
 import { AppContext } from './app'
 
 const hoverHighlight = 'rgba(147, 147, 147, 0.1)'
+const optionBaseStyle = { padding: '8px' }
 export const AppNavbar = () => {
     const { activePage, setActivePage } = useContext(AppContext)
     const [ hoverPage, setHoverPage ] = useState('')
 
     const NavbarOption: FC<{ icon: JSX.Element, label: string }> = ({ icon, label }) =>
         <Center
-            style={(hoverPage === label || activePage === label) ? { background: hoverHighlight } : undefined}
+            style={(hoverPage === label || activePage === label) ? { ...optionBaseStyle, background: hoverHighlight } : optionBaseStyle}
             onMouseOver={() => setHoverPage(label)}
             onMouseOut={() => setHoverPage('')}
             onClick={() => setActivePage(label)}
         >
             <ActionIcon size='xl'>{icon}</ActionIcon>
-            <Title style={{ marginLeft: '6px'}} order={6}>{label}</Title>
+            <Title style={{ marginLeft: '6px'}} order={5}>{label}</Title>
         </Center>
 
     return <Navbar width={{ base: 150 }} p='md'>
-        <NavbarOption icon={<MoodBoy size={50} />} label='people' />
-        <NavbarOption icon={<Location size={50} />} label='places' />
+        <NavbarOption icon={<MoodBoy size={40} />} label='people' />
+        <NavbarOption icon={<Location size={40} />} label='places' />
     </Navbar>
 }
